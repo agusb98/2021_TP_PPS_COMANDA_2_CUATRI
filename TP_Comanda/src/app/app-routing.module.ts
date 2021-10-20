29869133cfb128a099e6c1ac42eda67ddcbecd8e
agusb98 <agusszurdob@gmail.com>
2021-10-20 03:27:04 -0300

    creating splash page

--- TP_Comanda/src/app/app-routing.module.ts
+++ TP_Comanda/src/app/app-routing.module.ts
@@ -3,13 +3,14 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
+  { path: 'splash', loadChildren: () => import('./pages/splash/splash.module').then(m => m.SplashPageModule) },
   {
     path: 'home',
     loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule), canActivate: [AuthGuard]
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'splash',
     pathMatch: 'full'
   },
   { path: 'user', loadChildren: () => import('./pages/user/user.module').then(m => m.UserModule) },
