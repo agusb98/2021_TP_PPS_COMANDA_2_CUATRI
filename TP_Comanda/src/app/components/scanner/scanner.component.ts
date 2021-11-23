bcd975c1faea168fe1b58c864488463ba667401c
agusb98 <agusszurdob@gmail.com>
2021-11-22 22:16:47 -0300

    qrs

--- TP_Comanda/src/app/components/scanner/scanner.component.ts
+++ TP_Comanda/src/app/components/scanner/scanner.component.ts
@@ -4,9 +4,9 @@ import { PedidoService } from 'src/app/services/pedido.service';
 import { WaitListService } from 'src/app/services/wait.service';
 
 import { WaitList } from 'src/app/models/waitList';
-import { Pedido } from 'src/app/models/pedido';
 import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 
 @Component({
   selector: 'app-scanner',
@@ -19,11 +19,19 @@ export class ScannerComponent implements OnInit {
   public hasWait$: Observable<any>;
   public hasRequest$: Observable<any>;
 
+  private options = {
+    prompt: "Escaneá el QR",
+    formats: 'PDF_417, QR_CODE',
+    showTorchButton: true,
+    resultDisplayDuration: 2,
+  };
+
   constructor(
     private toastr: ToastrService,
     private router: Router,
     private waitService: WaitListService,
     private requestService: PedidoService,
+    private barcodeScanner: BarcodeScanner
   ) { }
 
   ngOnInit() {
@@ -47,6 +55,51 @@ export class ScannerComponent implements OnInit {
   }
 
   async scannQR() {
+    let data;
+    this.barcodeScanner.scan(this.options).then(barcodeData => {
+      const datos = barcodeData.text.split(' ');
+      data = { name: datos[0], id: datos[1], }
+    });
+
+    if (data) {
+      if (data.name == 'ENTRADA') {
+        this.hasWait$.subscribe(data => {
+          if (data.estado == 'PENDIENTE') {
+            this.toastr.error('Previamente usted ya solicitó una mesa, en breves se le acercará un recepcionista', 'Lista de espera');
+          }
+          else if (data.estado == 'EN USO') {
+            this.toastr.error('Usted ya tiene una mesa reservada, por favor consulte al empleado más cercano', 'Lista de espera');
+          }
+          else { this.addToWaitList(); }
+        });
+      }
+      else if (data.name == 'MESA') {
+        this.hasRequest$.subscribe(dataRes => {
+          
+          if (dataRes.mesa_numero == data.id) {
+            if (data.estado == 'PENDIENTE') {
+              this.router.navigate(['/producto/list']);
+            }
+            else if (data.estado == 'ACEPTADO') {
+              console.log('preguntarle si ya lo recibió');
+            }
+            else if (data.estado == 'CONFIRMADO') {
+              console.log('preguntarle si quiere pagar o jugar un jueguito');
+            }
+            else if (data.estado == 'COBRADO') {
+              console.log('preguntarle si quiere hacer una encuesta');
+            }
+          }
+          else {
+            this.toastr.error('Usted se ha equivocado de mesa', 'Lista de espera');
+          }
+        })
+      }
+      else if(data.name == 'PROPINA'){
+        console.log('preguntar si realmente quiere dejar propina');
+      }
+    }
+
 
     //obtener valor qr: puede ser para ingresar en lista de espera
     //o leer una mesa
@@ -55,31 +108,10 @@ export class ScannerComponent implements OnInit {
     // y varios qr de mesas
 
     //  if qr ir entrada
-    this.hasWait$.subscribe(data => {
-      if (data.estado == 'PENDIENTE') {
-        this.toastr.error('Previamente usted ya solicitó una mesa, en breves se le acercará un recepcionista', 'Lista de espera');
-      }
-      else if (data.estado == 'EN USO') {
-        this.toastr.error('Usted ya tiene una mesa reservada', 'Lista de espera');
-      }
-      else { this.addToWaitList(); }
-    })
+
 
     //  if qr is mesa X
-    this.hasRequest$.subscribe(data => {
-      if (data.estado == 'PENDIENTE') {
-        this.router.navigate(['/producto/list']);
-      }
-      else if (data.estado == 'ACEPTADO') {
-        console.log('preguntarle si ya lo recibió');
-      }
-      else if (data.estado == 'CONFIRMADO') {
-        console.log('preguntarle si quiere pagar o jugar un jueguito');
-      }
-      else if (data.estado == 'COBRADO') {
-        console.log('preguntarle si quiere hacer una encuesta');
-      }
-    })
+
   }
 
   private addToWaitList() {
